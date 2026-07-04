9de69ec4713ac1cc4fd9bbce076fec1f4c374655
Mitsuhiro Tanda <mitsuhiro.tanda@gmail.com>
2026-07-04 17:09:38 +0900

    difit: update pnpmDeps hash after nixpkgs/pnpm bump

--- pkgs/difit/default.nix
+++ pkgs/difit/default.nix
@@ -31,7 +31,7 @@ stdenv.mkDerivation (finalAttrs: {
 
   pnpmDeps = fetchPnpmDeps {
     inherit (finalAttrs) pname version src;
-    hash = "";
+    hash = "sha256-og4xTW7MJtBiOfanKIk6pjpmonnNmshfBNOSshT8xwQ=";
     fetcherVersion = 3;
   };
 
